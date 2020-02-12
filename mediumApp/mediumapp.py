38a4a68afe5b725d9af7981c6246c3e010372d02
yadav19 <dhiraj.yadav2873@gmail.com>
2020-02-13 02:33:34 +0530

    databse sqlite setup - models created User, Posts

--- mediumApp/mediumapp.py
+++ mediumApp/mediumapp.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, redirect, flash, url_for
 from forms import Register, Login
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
+from flask_bcrypt import Bcrypt
 app = Flask("__name__")
 app.config["SECRET_KEY"] = "9e881d1a94b2125d8df30ab584403075"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
@@ -23,6 +24,25 @@ posts = [
     }
 ]
 
+class Users(db.Model):
+    uid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(40), nullable=False, unique=True)
+    password = db.Column(db.String(20), nullable=False)
+    profile_pic = db.Column(db.String(30), nullable=False, default="default.jpg")
+    posts = db.relationship("Posts", backref="author",lazy=True) # 1. which model does it relate to //2. what will be reference name of it the other model //3. True means load the data necessary in one go
+
+    def __repr__(self):
+        return "User({}, {}, {}, {})".format(self.uid,self.username,self.email,self.profile_pic)
+
+class Posts(db.Model):
+    pid = db.Column(db.Integer, primary_key=True)
+    post_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    title = db.Column(db.String(40), nullable=True)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.uid"), nullable=False) # u in users is small as sqlite saves the tables in small letters
+    def __repr__(self):
+        return "Post({}, {}, {})".format(self.pid, self.post_date, self.title)
 
 @app.route("/")
 @app.route("/index")
@@ -45,6 +65,7 @@ def login():
 def register():
     form = Register()
     if form.validate_on_submit():
+
         flash(" you have successfully registered {}".format(form.username.data),"success")
         return redirect(url_for('index'))
     return render_template("register.html",title="REGISTRATION",form = form)
